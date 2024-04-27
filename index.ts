55ce65d331175775e80894e59ece69e59bbf5366
MarkRivera <Mrivera1991@gmail.com>
2024-04-26 21:07:56 -0400

    Created simple API

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+const todos: Array<{ title: string, checked: boolean }> = [];
+
+app.post('/api/todo/create', (req, res) => {
+    const { title, checked } = req.body;
+    todos.push({ title, checked });
+
+    res.send({ status: 200, message: 'Todo created successfully' });
+});
+
+app.get('/api/todo/read', (req, res) => {
+    res.json({ status: 200, data: todos });
+});
+
+app.delete('/api/todo/delete', (req, res) => {
+    const { title } = req.body;
+    const index = todos.findIndex(todo => todo.title === title);
+    todos.splice(index, 1);
+
+    res.send({ status: 200, message: 'Todo deleted successfully', data: todos });
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
