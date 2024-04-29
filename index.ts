e48c6d0b7089d7f6a93b7b23f23c07bbcc0108f5
YismelR <yismel_rodriguez@outlook.com>
2024-04-29 15:00:25 -0400

    added cors

--- index.ts
+++ index.ts
@@ -1,30 +1,38 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
-const todos: Array<{ title: string, checked: boolean }> = [];
-app.post('/api/todo/create', (req, res) => {
-    const { title, checked } = req.body;
-    todos.push({ title, checked });
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-    res.send({ status: 200, message: 'Todo created successfully' });
+app.use(express.json());
+
+const todos: Array<{ title: string; checked: boolean }> = [
+  { title: "Clean the kitchen", checked: false },
+];
+app.post("/api/todo/create", (req, res) => {
+  console.log(req.body);
+  const { title, checked } = req.body;
+  todos.push({ title, checked });
+
+  res.json({ status: 200, message: "Todo created successfully", data: todos });
 });
 
-app.get('/api/todo/read', (req, res) => {
-    res.json({ status: 200, data: todos });
+app.get("/api/todo/read", (req, res) => {
+  res.json({ status: 200, data: todos });
 });
 
-app.delete('/api/todo/delete', (req, res) => {
-    const { title } = req.body;
-    const index = todos.findIndex(todo => todo.title === title);
-    todos.splice(index, 1);
+app.delete("/api/todo/delete", (req, res) => {
+  const { title } = req.body;
+  const index = todos.findIndex((todo) => todo.title === title);
+  todos.splice(index, 1);
 
-    res.send({ status: 200, message: 'Todo deleted successfully', data: todos });
+  res.send({ status: 200, message: "Todo deleted successfully", data: todos });
 });
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
